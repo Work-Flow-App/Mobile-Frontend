656f258627f47bccddfa2faba56500b366db1f37
Ta-SeenJunaid <taseen.junaid@gmail.com>
2026-08-19 10:47:46 +0200

    fix(IOS+Android): update to latest version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ dartDefines?.split(",")?.forEach {
 
 android {
     namespace = "com.workfloow.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -45,7 +45,7 @@ android {
     defaultConfig {
         applicationId = "com.workfloow.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 
